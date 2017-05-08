2b6f9eb4b5b7ef532115932089e3d69d368f3955
rockstarchiku <jngamau93@gmail.com>
2017-05-08 15:05:49 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function() {
+    $("form#leap-year").submit(function(event) {
+      event.preventDefault();
+      var year = parseInt($("input#year").val());
+      var result = leapYear(year);
+      $("#result").text(result);
+    });
+  });
+  var leapYear = function(year) {
+      return false;
+    };
+  var leapYear = function(year) {
+        if (year % 4 === 0) {
+          return true;
+        } else {
+          return false;
+        }
+      };
